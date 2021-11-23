cc6a31cadfd089151c4263b15ee473f184f0f0a8
Francesca Collautti <94189769+francycolla01@users.noreply.github.com>
2021-11-23 16:56:08 +0000

    esercizio lezione 4

---
+++ lezione4.py
@@ -0,0 +1,50 @@
+#create un oggetto CSVFile che rappresenti un file CSVFile
+
+
+
+class CSVFile():
+    
+    #venga inizializzato sul nome del file csv e abbia un attributo name che contenga il nome 
+    def __init__(self, name):
+
+        self.name=name
+
+    #abbia un metodo get_data() che torni i dati del file CSV come lista di liste
+    def get_data(self):
+
+        #apro l'attributo name 
+        my_file=open(self.name, 'r')
+
+        #inizializzo lista vuota dove ci sono i valori
+        values=[]
+
+        #scorro tutte le linee
+        for line in my_file:
+
+            #divido tra Date  ,  Sales 
+            elements=line.split(',')
+
+            #aggiungo alla lista vuota le "liste piccole" create prima
+            values.append(elements)
+
+        #chiudo il file
+        my_file.close()
+
+        #ritorno la lista
+        return values
+
+
+#istanzio l'oggetto con il file shampoo_sales
+my_object=CSVFile('shampoo_sales.csv')
+
+#creo una variabile my_data che assumera' il "valore" dell'oggetto che mi ritorna get_data
+my_data=my_object.get_data()
+
+#stampo a schermo il risultato di get_data
+print(my_data)
+
+
+
+        
+    
+
