9146d2f40ea52245ffa3c682ca61e6eaebfd3a64
Francesca Collautti <94189769+francycolla01@users.noreply.github.com>
2021-11-28 12:20:44 +0000

    esercizio 1,  foglio1

--- foglio1/esercizio1.py
+++ foglio1/esercizio1.py
@@ -15,33 +15,78 @@ def stampa(the_list):
 #funzione statistiche
 def statistiche(the_list):
     
-    #inizializzo la somma 
+    #inizializzo la somma a 0
     somma=0
-    
+
     #valuto gli elementi della lista
     for item in the_list:
 
         #se gli elementi sono interi
-        if item%2==0:
+        if type(item)==int:
 
             #somma gli elementi
             somma=somma+item
 
+            #media degli elementi con funzione len()
+            media=somma/len(lista)
+
+            #trovo il minimo valore della lista con la funzione min()
+            minimo=min(lista)
+
+            #trovo il massimo valore della lista con la funzione max()
+            massimo=max(lista)
+
+
+    #ritorno i valori
+    print(somma)
+    print(media)  
+    print(minimo)
+    print(massimo)
+
+
+def somma_vettoriale(the_list, the_list2):
+
+    #inizializzo una terza lista vuota
+    lista3=[]
+
+    #valuto gli elementi della prima lista
+    for item in (range(len(the_list))):
+
+        #se gli elementi sono interi nella prima lista
+        if type(item)==int:
+
+            #se le liste hanno la stessa dimensione 
+            if len(the_list) == len(the_list2):
+
+                #effettuo la somma vettoriale
+                lista3=(the_list[item]+the_list2[item])
+
+                #stampo la nuova lista
+                print(lista3)
+
+        #se queste condizioni non sono soddisfatte
+        else:
+
+            #stampo lista vuota
+            print(lista3)
 
 
 
 
 
-
-#definizione lista
+#definizione liste
 lista=[1,2,3,4]
+lista2=[4,5,6,7]
 
 #richiamo la funzione 
 risultato=stampa(lista)
+risultato2=statistiche(lista)
+risultato3=somma_vettoriale(lista, lista2)
 
-#stampo risultato
+#stampo i risultati
 print(risultato)
-
+print(risultato2)
+print(risultato3)
 
 
 
