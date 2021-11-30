d1795a83e099a13910d0266bf715e6f99a8ae0c2
Francesca Collautti <94189769+francycolla01@users.noreply.github.com>
2021-11-30 15:51:11 +0000

    esercizio lezione 5 parte 2

--- lezione5_pt2.py
+++ lezione5_pt2.py
@@ -2,52 +2,107 @@
 
 #estendete l'oggetto CSVFile chiamandolo NumericalCSVFile e facendo in modo che converta automaticamente a numero tutte le colonne tranne la prima (della data)
 
-class CSVFile():
-    
-    #venga inizializzato sul nome del file csv e abbia un attributo name che contenga il nome 
+class CSVFile:
+
     def __init__(self, name):
-
-        self.name=name
-
-    class NumericalCSVFile(CSVFil):
-
-        #apro shampoo_sales
-        my_file=open('shampoo_sales.csv', 'r')
-
-        def get_data(self):
-
-            #scorro tutte le linee
-            for line in my_file:
-
-                #divido tra Date  ,  Sales 
-                elements=line.split(',')
-
-                #se non sono su Date
-                if elements[0]!="Date":
         
-                    #definisco date nella stringa alla prima posizione
-                    date=elements[0]
-
-                    #definisco value nella stringa alla seconda posizione
-                    value=elements[1]
-
-                    #copio value e lo trasformo in un float
-                    values.append(float(value))
-
-                    return values
-
-#chiudo il file
-my_file.close()
-
+        # Setto il nome del file
+        self.name = name
+        
+        
+        # Provo ad aprirlo e leggere una riga
+        self.can_read = True
+        try:
+            my_file = open(self.name, 'r')
+            my_file.readline()
+        except Exception as e:
+            self.can_read = False
+            print('Errore in apertura del file: "{}"'.format(e))
 
 
-#poi aggiungete questi due campi al file "shampoo_sales.csv" (vedi pdf) e gestite gli errori che verranno generati in modod che le linee vengano saltate senza bloccare il programma ma che venga stampato a scehrmo l'errore
+    def get_data(self):
+        
+        if not self.can_read:
+            
+            # Se nell'init ho settato can_read a False vuol dire che il file non poteva essere aperto o era illeggibile
+            print('Errore, file non aperto o illeggibile')
+            
+            # Esco dalla funzione tornando "niente".
+            return None
 
-#istanzio l'oggetto con il file shampoo_sales
-my_object=CSVFile('shampoo_sales.csv')
+        else:
+            # Inizializzo una lista vuota per salvare tutti i dati
+            data = []
+    
+            # Apro il file
+            my_file = open(self.name, 'r')
 
-#creo una variabile my_data che assumera' il "valore" dell'oggetto che mi ritorna get_data
-my_data=my_object.get_data()
+            # Leggo il file linea per linea
+            for line in my_file:
+                
+                # Faccio lo split di ogni linea sulla virgola
+                elements = line.split(',')
+                
+                # Posso anche pulire il carattere di newline dall'ultimo elemento con la funzione strip():
+                elements[-1] = elements[-1].strip()
+                
+                # p.s. in realta' strip() toglie anche gli spazi bianchi all'inizio e alla fine di una stringa.
+    
+                # Se NON sto processando l'intestazione...
+                if elements[0] != 'Date':
+                        
+                    # Aggiungo alla lista gli elementi di questa linea
+                    data.append(elements)
+            
+            # Chiudo il file
+            my_file.close()
+            
+            # Quando ho processato tutte le righe, ritorno i dati
+            return data
 
-#stampo a schermo il risultato di get_data
-print(my_data)
+            
+class NumericalCSVFile(CSVFile):
+    
+    def get_data(self):
+        
+        # Chiamo la get_data del genitore 
+        string_data = super().get_data()
+        
+        # Preparo lista per contenere i dati ma in formato numerico
+        numerical_data = []
+        
+        # Ciclo su tutte le "righe" corrispondenti al file originale 
+        for string_row in string_data:
+            
+            # Preparo una lista di supporto per salvare la riga in "formato" nuumerico (tranne il primo elemento)
+            numerical_row = []
+            
+            # Ciclo su tutti gli elementi della riga con un enumeratore: cosi' ho gratis l'indice "i" della posizione dell'elemento nella riga.
+            for i,element in enumerate(string_row):
+                
+                if i == 0:
+                    # Il primo elemento della riga lo lascio in formato stringa
+                    numerical_row.append(element)
+                    
+                else:
+                    # Converto a float tutto gli altri. Ma se fallisco, stampo l'errore e rompo il ciclo (e poi saltero' la riga).
+                    try:
+                        numerical_row.append(float(element))
+                    except Exception as e:
+                        print('Errore in conversione del valore "{}" a numerico: "{}"'.format(element, e))
+                        break
+                
+            # Alla fine aggiungo la riga in formato numerico alla lista "esterna", ma solo se sono riuscito a processare tutti gli elementi. Qui controllo per la lunghezza, ma avrei anche potuto usare una variabile di supporto o fare due break in cascata.
+            if len(numerical_row) == len(string_row):
+                numerical_data.append(numerical_row)
+
+        return numerical_data
+
+mio_file = CSVFile(name='shampoo_sales.csv')
+print('Nome del file: "{}"'.format(mio_file.name))
+print('Dati contenuti nel file: "{}"'.format(mio_file.get_data()))
+
+
+mio_file_numerico = NumericalCSVFile(name='shampoo_sales.csv')
+print('Nome del file: "{}"'.format(mio_file_numerico.name))
+print('Dati contenuti nel file: "{}"'.format(mio_file_numerico.get_data()))
